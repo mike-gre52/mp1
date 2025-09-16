e76026d279f282da77648087acdd803b03f315de
mike greene <greenemike52@gmail.com>
2025-09-16 13:44:34 -0400

    added comments

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+// HTML Elements accessed through DOM
 let one=document.getElementById("one");
 let two=document.getElementById("two");
 let output=document.getElementById("output");
+
+// Add Function
 function doAdd(){
     output.innerHTML = "= " + String(Number(one.value) + Number(two.value));
 }
 
+// Subtract Function
 function doSub(){
     output.innerHTML = "= " +String(Number(one.value) - Number(two.value));
 }
 
+// Multiply Function
 function doMultiply(){
     output.innerHTML = "= " + String(Number(one.value) * Number(two.value));
 }
 
+// Divide Function
 function doDivide(){
     output.innerHTML = "= " +String(Number(one.value) / Number(two.value));
 }
 
+// Power Function
 function doPower(){
     let result = Number(one.value)
     for (let i =0; i < Number(two.value) - 1; i++) {
@@ -27,6 +34,7 @@ function doPower(){
     output.innerHTML = "= " + String(result);
 }
 
+// Clear Function
 function doClear(){
     one.value = String("")
     two.value = String("")
